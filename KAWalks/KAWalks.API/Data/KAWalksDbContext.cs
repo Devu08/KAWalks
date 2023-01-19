db1d5cdd3033f0f4a931c645b69b1b7082b6b85a
devika <devikamurali08@gmail.com>
2023-01-19 15:45:07 +0530

    End of section 2

---
+++ KAWalks/KAWalks.API/Data/KAWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using KAWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace KAWalks.API.Data
+{
+    
+    public class KAWalksDbContext : DbContext
+    {
+        public KAWalksDbContext(DbContextOptions <KAWalksDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region>Regions { get; set; }
+        public DbSet<Walk>Walks { get; set; }
+        public DbSet<WalkDifficulty>WalkDifficulty { get; set; }
+    }
+  
+
+}
---
+++ KAWalks/KAWalks.API/Migrations/20230119101206_InitialMigration.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KAWalks.API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lat = table.Column<double>(type: "float", nullable: false),
+                    Long = table.Column<double>(type: "float", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    Population = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WalkDifficulty",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WalkDifficulty", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<double>(type: "float", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WalkDifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegionWalk",
+                columns: table => new
+                {
+                    RegionsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WalksId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegionWalk", x => new { x.RegionsId, x.WalksId });
+                    table.ForeignKey(
+                        name: "FK_RegionWalk_Regions_RegionsId",
+                        column: x => x.RegionsId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RegionWalk_Walks_WalksId",
+                        column: x => x.WalksId,
+                        principalTable: "Walks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegionWalk_WalksId",
+                table: "RegionWalk",
+                column: "WalksId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegionWalk");
+
+            migrationBuilder.DropTable(
+                name: "WalkDifficulty");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+
+            migrationBuilder.DropTable(
+                name: "Walks");
+        }
+    }
+}
